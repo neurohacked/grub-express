840f33832c484585e771e4adf5ef396c97c4ce8f
neurohacked <admin@neurohacked.com>
2016-09-24 11:49:09 -0700

    fix arrays

---
+++ app/data/waitinglist-data.js
@@ -0,0 +1,12 @@
+var waitlist = [
+
+{
+  customerName: "Nathaniel Van Doren",
+  phoneNumber: "4249036307",
+  customerEmail: "Spider.Byt3s@gmail.com",
+  customerID: "Nathaniel Van Doren"
+}
+
+];
+
+module.exports = waitlist;
