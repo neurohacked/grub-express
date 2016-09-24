9b7db400582ebf9db7c7bcb5efb5b5da46ac4f27
Hannah Marlin <hmmarlin@gmail.com>
2016-09-24 11:36:38 -0700

    remove grub

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -3,7 +3,7 @@ var path = require('path');
 
 module.exports = function(app){
 	app.get('/api/tables', function(req, res){
-		res.json(grub);
+		res.json(reservations);
 	});
 
 	app.post('/api/waitlist', function(req, res){
