3b23261966b7076b5f62036838365989f868a0fc
Hannah Marlin <hmmarlin@gmail.com>
2016-09-24 11:21:21 -0700

    require route js files

--- server.js
+++ server.js
@@ -11,11 +11,14 @@ app.listen(PORT, function () {
 	console.log('App listening on PORT ' + PORT);
 });
 
+// Links to our html routes
+
+require('./app/routing/routes-api.js')(app);
+require('./app/routing/routes-html.js')(app);
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
-
 
