c92943d3fb98de0904fc5a99a9d3d6c8134b8fbb
neurohacked <admin@neurohacked.com>
2016-09-24 13:49:45 -0700

    Cleanup code
    Remove comments, etc.

--- app/data/table-data.js
+++ app/data/table-data.js
@@ -1,12 +1,4 @@
 var tables = [
-
-{
-  customerName: "Guy Pham",
-  phoneNumber: "4249036307",
-  customerEmail: "Spider.Byt3s@gmail.com",
-  customerID: "Guy Pham"
-}
-
 ];
 
 module.exports = tables;
